0b21e2483beef2839e5a6937feb92fea3d332729
luismeza8 <angel841bv@gmail.com>
2024-02-28 09:00:34 -0700

    28 feb

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -1,7 +1,12 @@
+
+<?php
+    $titulo = "Dashboard";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>AdminLTE 3 | Dashboard 3</title>
+<title>POS API - <?= $titulo; ?></title>
     <?php include_once("../app/Views/plantillas/head.php");?>
 </head>
 <!--
@@ -28,12 +33,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard v3</h1>
+          <h1 class="m-0"><?= $titulo?></h1>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v3</li>
+              <li class="breadcrumb-item active"><?= $titulo?></li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
